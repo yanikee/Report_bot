a7fb98d9319c75381bbdc6388cf0bfa33a4a3207
yanike <yanike.82k@gmail.com>
2026-04-19 16:11:01 +0900

    resetコマンド追加

---
+++ cogs/manage/reset.py
@@ -0,0 +1,99 @@
+from discord.ext import commands
+from discord import app_commands, ui
+import discord
+
+from const import EMOJI_DICT
+from modules.db import DB, GuildSettings
+from modules.error import send_error
+
+
+
+class Reset(commands.Cog):
+  def __init__(self, bot: commands.Bot):
+    self.bot = bot
+    self.db = DB()
+    self.data: dict[int, GuildSettings] = {}
+
+
+  @app_commands.command(name="reset", description='サーバーの設定をリセットします。')
+  @discord.app_commands.guild_only()
+  async def reset(self, interaction: discord.Interaction):
+    guild = interaction.guild
+    if not guild:
+      return
+
+    await interaction.response.defer(ephemeral=True)
+
+    guild_data = await self.db.get_guild_settings(guild.id)
+    if not guild_data:
+      guild_data = await self.db.create_guild_settings(guild.id)
+
+    self.data[guild.id] = guild_data
+
+    view = self.confirm_reset()
+    await interaction.followup.send(view=view)
+
+
+  def confirm_reset(self) -> ui.LayoutView:
+    container = ui.Container(accent_color=0xff0000)
+    container.add_item(ui.TextDisplay("サーバーの設定をリセットします。よろしいですか？"))
+    row = ui.ActionRow()
+    row.add_item(ui.Button(label="はい", custom_id=f"reset_yes", style=discord.ButtonStyle.red))
+    row.add_item(ui.Button(label="いいえ", custom_id=f"reset_no", style=discord.ButtonStyle.gray))
+    container.add_item(row)
+
+    view = ui.LayoutView()
+    view.add_item(container)
+    return view
+
+
+  @commands.Cog.listener()
+  async def on_interaction(self, interaction: discord.Interaction):
+    guild = interaction.guild
+    if not guild:
+      return
+
+    message = interaction.message
+    if not message:
+      return
+
+
+    if not interaction.data:
+      return
+
+    custom_id = interaction.data.get("custom_id")
+    if not custom_id:
+      return
+
+    # page
+    if "reset_yes" in custom_id:
+      await interaction.response.defer()
+
+      data = {
+        "guild_id": guild.id,
+        "report_channel_id": None,
+        "report_mention_role_id": None,
+        "ticket_channel_id": None,
+        "ticket_button_channel_id": None,
+        "ticket_mention_role_id": None,
+      }
+
+      await self.db.upsert_guild_settings(data)
+
+      container = ui.Container(accent_color=0x00ff00)
+      container.add_item(ui.TextDisplay("リセットしました。`/settings`コマンドで再設定をしましょう。"))
+
+      view = ui.LayoutView()
+      view.add_item(container)
+      await interaction.edit_original_response(view=view)
+      return
+
+    if "reset_no" in custom_id:
+      await interaction.response.defer()
+      await interaction.delete_original_response()
+      return
+
+
+
+async def setup(bot):
+  await bot.add_cog(Reset(bot))
